be5c721dcd580dde89016c0172c0a3a925aab87c
Christoph Könekamp <55452146+christophkoenekamp@users.noreply.github.com>
2021-07-06 01:28:18 +0200

    Create GlobalFeed.js

---
+++ querying/GlobalFeed.js
@@ -0,0 +1,106 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const APIURL = "https://api.thegraph.com/subgraphs/name/username/subgraphname";
+
+const tasksQuery = `
+  query {
+    tasks(orderBy: amount, orderDirection: desc) {
+      id
+      initiatorAddress
+      recipientAddress
+      amount
+      entranceAmount
+      description
+      endTask
+      positiveVotes
+      negativeVotes
+      participants
+      accepted
+      executed
+      finished
+      canceled
+      hashtag1
+      hashtag2
+      hashtag3
+      language
+      proofLink
+    }
+  }
+`
+
+const userTasksQuery = `
+  query {
+    userTasks {
+      id
+      userAddress
+      userStake
+      voted
+      vote
+      taskData
+    }
+  }
+`
+
+const betsQuery = `
+  query {
+    bets(orderBy: stakeA & stakeB, orderDirection: desc) {
+      id
+      initiatorAddress
+      description
+      participantsA
+      participantsB
+      stakeA
+      stakeB
+      textA
+      textB
+      endBet
+      noMoreBets
+      finished
+      winnerPartyA
+      draw
+      failed
+      hashtag1
+      hashtag2
+      hashtag3
+      language
+      proofLink
+    }
+  }
+`
+
+const userBetsQuery = `
+  query {
+    userBets {
+      id
+      userAddress
+      userStake
+      redeemed
+      joinedA
+      betData
+    }
+  }
+`
+
+const globalStatsQuery = `
+  query {
+    globalStats {
+      id
+      taskProfits
+      users
+      taskCount
+      betProfit
+      betCount
+    }
+  }
+`
+
+const client = new ApolloClient({
+  uri: APIURL,
+  cache: new InMemoryCache()
+});
+
+client.query({
+  query: gql(tasksQuery, userTasksQuery, betsQuery, userBetsQuery, userFavStatsQuery, userDashStatsQuery, userAchievementsQuery, globalStatsQuery)
+})
+.then(data => console.log("Subgraph data: ", data))
+.catch(err => { console.log("Error fetching data: ", err) });

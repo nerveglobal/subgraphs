4cc8b91fb42899aa7eb728120c7f30ce1a6cdaf4
Christoph Könekamp <55452146+christophkoenekamp@users.noreply.github.com>
2021-07-08 22:36:25 +0200

    Create UserDashStatsQuery.js

---
+++ querying/UserDashStatsQuery.js
@@ -0,0 +1,32 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const APIURL = "https://api.thegraph.com/subgraphs/name/username/subgraphname";
+
+const userDashStatsQuery = `
+  query {
+    userDashStats {
+      id
+      userName
+      displayAchievement
+      youtube
+      twitter
+      instagram
+      tiktok
+      twitch
+      tribute
+      profit
+      blacklist
+    }
+  }
+`
+
+const client = new ApolloClient({
+  uri: APIURL,
+  cache: new InMemoryCache()
+});
+
+client.query({
+  query: gql(userDashStatsQuery)
+})
+.then(data => console.log("Subgraph data: ", data))
+.catch(err => { console.log("Error fetching data: ", err) });

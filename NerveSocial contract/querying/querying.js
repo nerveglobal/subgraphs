8c5d42587af4a0ed0eede09493da826529e61081
Christoph Könekamp <55452146+christophkoenekamp@users.noreply.github.com>
2021-07-29 03:50:38 +0200

    Update querying.js

--- NerveSocial contract/querying/querying.js
+++ NerveSocial contract/querying/querying.js
@@ -1,1 +1,30 @@
-test
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const APIURL = "https://api.thegraph.com/subgraphs/name/username/subgraphname";
+
+const userDashStatsQuery = `
+  query {
+    userDashStats {
+      id
+      userName
+      displayAchievement
+      youtube
+      twitter
+      instagram
+      tiktok
+      twitch
+      blacklist
+    }
+  }
+`
+
+const client = new ApolloClient({
+  uri: APIURL,
+  cache: new InMemoryCache()
+});
+
+client.query({
+  query: gql(userDashStatsQuery)
+})
+.then(data => console.log("Subgraph data: ", data))
+.catch(err => { console.log("Error fetching data: ", err) });

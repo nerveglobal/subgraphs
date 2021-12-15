8500d07be02e766e82919a80b380101b0f7eda0b
Christoph Könekamp <55452146+christophkoenekamp@users.noreply.github.com>
2021-12-15 22:26:00 +0100

    new contract + subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -13,7 +13,11 @@ import {
   BetFinished,
   BetJoined,
   BetProved,
-  BetRedeemed
+  BetRedeemed,
+  NameRegistered,
+  SocialRegistered,
+  LocationRegistered,
+  UserBlacklisted
 } from "../generated/NerveGlobal/NerveGlobal"
 import { 
   Task, 
@@ -22,7 +26,8 @@ import {
   UserBet,
   UserFavStat,
   UserDashStat,
-  GlobalStat
+  GlobalStat,
+  UserSocialStat
 } from "../generated/schema"
 
 
@@ -78,7 +83,7 @@ export function handleTaskAdded(event: TaskAdded): void {
   let globalStat = GlobalStat.load(globalStatId)
   if(globalStat == null) {
     globalStat = new GlobalStat(globalStatId)
-    log.info('New GlobalStat entity created: {} - {}', [globalStatId])
+    log.info('New GlobalStat entity created: {}', [globalStatId])
   }
   globalStat.taskCount = globalStat.taskCount.plus(BigInt.fromI32(1)) 
   globalStat.save()
@@ -494,3 +499,119 @@ export function handleBetProved(event: BetProved): void {
   bet.proofLink = event.params.proofLink
   bet.save()
 }
+
+  /******************************************/
+  /*             NameRegistered             */
+  /******************************************/
+
+  export function handleNameRegistered(event: NameRegistered): void {
+
+    let user = event.params.user.toHex()
+      
+    //  UserDashStat Entity
+    let userDashStat = UserDashStat.load(user)
+    if(userDashStat == null) {
+      userDashStat = new UserDashStat(user)
+      log.info('New UserDashStat entity created: {}', [user])
+    }
+  
+    // GlobalStats Entity 
+    if(userDashStat.userName == "") {                                                                  
+      let globalStatId = "1"
+      let globalStat = GlobalStat.load(globalStatId)
+      if(globalStat == null) {
+        globalStat = new GlobalStat(globalStatId)
+        log.info('New GlobalStat entity created: {}', [globalStatId])
+      }
+      globalStat.users = globalStat.users.plus(BigInt.fromI32(1)) 
+      globalStat.save()
+    }
+  
+    userDashStat.userName = event.params.registeredName.toString()
+    userDashStat.save()     
+  }
+  
+    
+    /******************************************/
+    /*            SocialRegistered            */
+    /******************************************/
+  
+  export function handleSocialRegistered(event: SocialRegistered): void {
+    
+    let user = event.params.user.toHex()
+    
+    //  UserSocialStat Entity
+    let userSocialStat = UserSocialStat.load(user)
+    if(userSocialStat == null) {
+      userSocialStat = new UserSocialStat(user)
+      log.info('New UserSocialStat entity created: {}', [user])
+    } 
+  
+    //let socialLinks: string[] = event.params.socialLinks;
+    let socialLinks = event.params.socialLinks;
+    let socialIds = event.params.socialIds;
+  
+    log.info('links: {}', socialLinks);
+  
+    //let socialIds: BigInt[] = event.params.socialIds;
+    for(var i = 0; i < socialIds.length; i++)
+    { 
+  
+      log.info('Social link: {}', [socialLinks[i]]);
+      log.info('Social ID: {}', [socialIds[i]]);
+      if(socialIds[i] == "1")
+        userSocialStat.instagram = socialLinks[i]
+      if(socialIds[i] == "2")
+        userSocialStat.twitter = socialLinks[i]
+      if(socialIds[i] == "3")
+        userSocialStat.tiktok = socialLinks[i]
+      if(socialIds[i] == "4")
+        userSocialStat.twitch = socialLinks[i]
+      if(socialIds[i] == "5")
+        userSocialStat.youtube = socialLinks[i]
+    }
+    userSocialStat.save()                                                         
+  }
+  
+  /******************************************/
+  /*             LocationRegistered         */
+  /******************************************/
+  
+  export function handleLocationRegistered(event: LocationRegistered): void {
+  
+    let user = event.params.user.toHex()
+    
+    //  UserSocialStat Entity
+    let userSocialStat = UserSocialStat.load(user)
+    if(userSocialStat == null) {
+      userSocialStat = new UserSocialStat(user)
+      log.info('New UserSocialStat entity created: {}', [user])
+    }
+    if(event.params.latitude == BigInt.fromI32(1))
+    userSocialStat.userLatitude = event.params.latitude.toString()
+    if(event.params.longitude == BigInt.fromI32(2))
+    userSocialStat.userLongitude = event.params.longitude.toString()
+    userSocialStat.save()                                                            
+  }
+  
+  
+    /******************************************/
+    /*            UserBlacklisted             */
+    /******************************************/
+  
+  export function handleUserBlacklisted(event: UserBlacklisted): void {
+  
+    let user = event.params.user.toHex()
+      
+    //  UserSocialStat Entity
+    let userSocialStat = UserSocialStat.load(user)
+    if (userSocialStat === null) {
+      userSocialStat = new UserSocialStat(user);
+    }
+    if(userSocialStat == null) {
+      userSocialStat = new UserSocialStat(user)
+      log.info('New UserSocialStat entity created: {}', [user])
+    }
+    userSocialStat.blacklist.push(event.params.userToBlacklist)
+    userSocialStat.save()                                                         
+  }
